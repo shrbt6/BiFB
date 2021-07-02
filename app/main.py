d9b13912062e480fb69302c9c3bab635a6c63aab
yochimonji <yochimonji@gmail.com>
2021-07-02 22:48:19 +0900

    add #3 testを追加

--- app/main.py
+++ app/main.py
@@ -1,13 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def hello():
-    return "Hello World!"
+    return 'Hello World!'
 
-@app.route("/hello")
+@app.route('/test')
 def hello1():
-    return "こんにちは"
+    return render_template('test.html')
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
