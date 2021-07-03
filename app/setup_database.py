ee87823528a7ba83644ddb2264e971ef1f77459e
yochimonji <yochimonji@gmail.com>
2021-07-04 01:44:08 +0900

    fix #19 エラーを修正した

--- app/setup_database.py
+++ app/setup_database.py
@@ -3,27 +3,28 @@ from app_sqlite import exec
 exec('''
 CREATE TABLE users (
     user_id     TEXT PRIMARY KEY,
-    point       INTEGER default 1;
+    point       INTEGER default 1
 )
 ''')
 
 exec('''
 CREATE TABLE apps (
     app_id      INTEGER PRIMARY KEY AUTOINCREMENT,
-    user_id     TEXT PRIMARY KEY,
+    user_id     TEXT,
     title       TEXT,
     description TEXT,
     url         TEXT,
     filename    TEXT,
-    created_at  TIMESTAMP DEFAULT (DATETIME('now', 'localtime'))
-    limit       TIMESTAMP DEFAULT (DATETIME('now', 'localtime')) + INTEGER '30'
+    created_at  TIMESTAMP DEFAULT (DATETIME('now', 'localtime')),
+    limit_at    TIMESTAMP DEFAULT (DATETIME('now', 'localtime', '+30 days'))
 )
 ''')
 
 exec('''
 CREATE TABLE feedback (
     feedback_id     INTEGER PRIMARY KEY AUTOINCREMENT,
-    user_id         TEXT PRIMARY KEY,
+    user_id         TEXT,
+    app_id          INTEGER,
     title           TEXT,
     description     TEXT,
     created_at      TIMESTAMP DEFAULT (DATETIME('now', 'localtime'))
