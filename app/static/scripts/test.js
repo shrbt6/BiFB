0a8d15be6b5b90242f38449a2756afb46d01000a
yochimonji <yochimonji@gmail.com>
2021-07-03 12:30:52 +0900

    remove #11 testフォルダを削除

--- app/static/scripts/test.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function(){
-    let testCount = 0;
-    let testText = document.getElementById('test-text')
-
-    document.getElementById('test-button').addEventListener('click', function() {
-        testCount += 1;
-        console.log(testCount);
-    }, false)
-}, false)
