3b3b3a051c223d3c9a30d2831feb8e600771338d
yochimonji <yochimonji@gmail.com>
2021-07-03 21:29:59 +0900

    update setup_database.pyを更新

--- app/setup_database.py
+++ app/setup_database.py
@@ -1,20 +1,29 @@
 from app_sqlite import exec
 
 exec('''
+CREATE TABLE users (
+    user_id     TEXT PRIMARY KEY,
+    point       INTEGER default 1;
+)
+''')
+
+exec('''
 CREATE TABLE apps (
     app_id      INTEGER PRIMARY KEY AUTOINCREMENT,
-    user_id     INTEGER,
+    user_id     TEXT PRIMARY KEY,
     title       TEXT,
     description TEXT,
     url         TEXT,
     filename    TEXT,
     created_at  TIMESTAMP DEFAULT (DATETIME('now', 'localtime'))
+    limit       
 )
 ''')
 
 exec('''
 CREATE TABLE feedback (
     feedback_id     INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_id         TEXT PRIMARY KEY,
     title           TEXT,
     description     TEXT,
     created_at      TIMESTAMP DEFAULT (DATETIME('now', 'localtime'))
